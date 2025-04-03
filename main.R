b385a09e8fc473c6ef5317c1a1c201698ec74ada
Guadalupe Nibeyro <guadalupenibeyro@Guadalupes-MacBook-Air.local>
2025-04-03 16:20:50 -0300

    add instruction to check working directory

--- main.R
+++ main.R
@@ -1,7 +1,9 @@
+#Make sure to set the working directory into Gene_Fusion_ICB folder
+
 #load data
-Table_S1 <- read.csv("~/Gene_Fusion_ICB/data/Table_S1.csv")
-Table_S2 <- read.csv("~/Gene_Fusion_ICB/data/Table_S2.csv")
-non_ICB_Cohort <- read.csv("~/Gene_Fusion_ICB/data/non_ICB_Cohort.csv")
+Table_S1 <- read.csv("data/Table_S1.csv")
+Table_S2 <- read.csv("data/Table_S2.csv")
+non_ICB_Cohort <- read.csv("data/non_ICB_Cohort.csv")
 
 #load libraries
 library(ggplot2)
@@ -14,12 +16,12 @@ library(epiR)
 library(gridExtra)
 library(smoothROCtime)
 
-source("~/Gene_Fusion_ICB/R/Fig_1A_lm.R", echo=F)
-source("~/Gene_Fusion_ICB/R/Fig_1B.R", echo=F)
-source("~/Gene_Fusion_ICB/R/Fig_1C.R", echo=F)
-source("~/Gene_Fusion_ICB/R/Fig_1D.R", echo=F)
-source("~/Gene_Fusion_ICB/R/tdROC.R", echo=F)
-source("~/Gene_Fusion_ICB/R/Fig_2AB.R", echo=F)
-source("~/Gene_Fusion_ICB/R/Fig_3A.R", echo=F)
-source("~/Gene_Fusion_ICB/R/Fig_3B.R", echo=F)
+source("R/Fig_1A_lm.R", echo=F)
+source("R/Fig_1B.R", echo=F)
+source("R/Fig_1C.R", echo=F)
+source("R/Fig_1D.R", echo=F)
+source("R/tdROC.R", echo=F)
+source("R/Fig_2AB.R", echo=F)
+source("R/Fig_3A.R", echo=F)
+source("R/Fig_3B.R", echo=F)
 
