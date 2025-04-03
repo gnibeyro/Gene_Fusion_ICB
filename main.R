c9e9fc25b4262b508cf66c4eaeb58dd36f0a4f1b
Guadalupe Nibeyro <guadalupenibeyro@Guadalupes-MacBook-Air.local>
2025-04-03 11:48:41 -0300

    update paths

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 #load data
-Table_S1 <- read.csv("Data/Table_S1.csv")
-Table_S2 <- read.csv("Data/Table_S2.csv")
-non_ICB_Cohort <- read.csv("Data/non_ICB_Cohort.csv")
+Table_S1 <- read.csv("~/Gene_Fusion_ICB/data/Table_S1.csv")
+Table_S2 <- read.csv("~/Gene_Fusion_ICB/data/Table_S2.csv")
+non_ICB_Cohort <- read.csv("~/Gene_Fusion_ICB/data/non_ICB_Cohort.csv")
 
 #load libraries
 library(ggplot2)
@@ -14,12 +14,12 @@ library(epiR)
 library(gridExtra)
 library(smoothROCtime)
 
-source("~/Documents/Gene_Fusion_ICB/R/Fig_1A_lm.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/Fig_1B.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/Fig_1C.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/Fig_1D.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/tdROC.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/Fig_2AB.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/Fig_3A.R", echo=F)
-source("~/Documents/Gene_Fusion_ICB/R/Fig_3B.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_1A_lm.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_1B.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_1C.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_1D.R", echo=F)
+source("~/Gene_Fusion_ICB/R/tdROC.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_2AB.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_3A.R", echo=F)
+source("~/Gene_Fusion_ICB/R/Fig_3B.R", echo=F)
 
