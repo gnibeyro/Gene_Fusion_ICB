a36dcbcb4150d2078e6203bf92ba0b3804778923
Guadalupe Nibeyro <guadalupenibeyro@Guadalupes-MacBook-Air.local>
2025-04-03 10:59:27 -0300

    add set.seed

--- R/tdROC.R
+++ R/tdROC.R
@@ -1,10 +1,10 @@
 DB_TX <- Table_S1[-which(Table_S1$LS == "Dead (not melanoma)"),]
 DB_TX <- DB_TX[-which(is.na(DB_TX$OS) == T),]
 
-library(smoothROCtime)
 data <- DB_TX[,c("OS","V","TFB")]
 data <- as.matrix(data)
 
+set.seed(265)
 AUCbt3 <- 1:100
 for(i in 1:100) {
   id_train <- sample(1:146, replace = TRUE)
@@ -15,8 +15,10 @@ for(i in 1:100) {
 
 AUC3 <- as.numeric(AUCbt3)
 
-sum(AUC3)/100
+print("Mean AUC 100 bootstrap iterations")
+print(sum(AUC3)/100)
 
-wilcox.test(AUC3, mu=0.5, alternative = "greater" )
+print("Statistical significance")
+print(wilcox.test(AUC3, mu=0.5, alternative = "greater" ))
 
-boxplot(AUC3)
+boxplot(AUC3, main = "AUC by iteration distribution")
