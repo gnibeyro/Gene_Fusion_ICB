e3369d75afe9b13bc9fcc142e56079a60463a35d
Guadalupe Nibeyro <guadalupenibeyro@Guadalupes-MacBook-Air.local>
2025-06-18 09:42:17 -0300

    change order

--- R/Fig1B_lm.R
+++ R/Fig1B_lm.R
@@ -9,14 +9,6 @@ ca20_Fus <- ggscatter(Table_S1, x = "CA20", y = "TFB",
 print("Figure 1B: Cromosomal Instability")
 print(ca20_Fus)
 
-Prolif_Fus <- ggscatter(Table_S1[-174,], x = "Prolif", y = "TFB",
-                        color = "Cohort", palette = "jco",
-                        add = "reg.line", conf.int = F)+
-  geom_hline(yintercept = 11)+
-  ggtitle("Proliferation")
-print("Figure 1B: Proliferation")
-print(Prolif_Fus)
-
 CYT_Fus <- ggscatter(Table_S1[-c(78,136),], x = "CYT", y = "TFB",
                      color = "Cohort", palette = "jco",
                      add = "reg.line", conf.int = F)+
@@ -25,11 +17,21 @@ CYT_Fus <- ggscatter(Table_S1[-c(78,136),], x = "CYT", y = "TFB",
 print("Figure 1B: Cytolityc Activity")
 print(CYT_Fus)
 
-print("Linear Model: Proliferation")
-print(summary(lm(TFB ~ Prolif+Cohort, Table_S1[-174,])))
+Prolif_Fus <- ggscatter(Table_S1[-174,], x = "Prolif", y = "TFB",
+                        color = "Cohort", palette = "jco",
+                        add = "reg.line", conf.int = F)+
+  geom_hline(yintercept = 11)+
+  ggtitle("Proliferation")
+print("Figure 1B: Proliferation")
+print(Prolif_Fus)
+
+print("Linear Model: Cromosomal Instability")
+print(summary(lm(TFB ~ CA20+Cohort, Table_S1)))
 
 print("Linear Model: Cytolityc Activity")
 print(summary(lm(TFB ~ CYT+Cohort, Table_S1[-c(78,136),])))
 
-print("Linear Model: Cromosomal Instability")
-print(summary(lm(TFB ~ CA20+Cohort, Table_S1)))
+print("Linear Model: Proliferation")
+print(summary(lm(TFB ~ Prolif+Cohort, Table_S1[-174,])))
+
+
